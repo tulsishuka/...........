154f97b5f1d44f26c2c1ad12e94876460cef40a5
Tulasi <tulsishuklag@gmail.com>
2026-04-07 14:37:15 +0530

    backend/models/productModel.ts

--- backend/app.ts
+++ backend/app.ts
@@ -5,13 +5,16 @@ import productRouter from "./routes/productRouter";
 import orderRouter from "./routes/orderRouter";
 import cartRouter from "./routes/cart.routes";
 import cors from "cors";
+import path from "path";
+
 
 const app = express();
 
 app.use(express.json());
 
 app.use(cors());
- 
+ app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
+
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/product", productRouter);
 app.use("/api/v1/order", orderRouter);
